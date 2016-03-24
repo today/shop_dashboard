11a53b2891a523ee31e22577bbbffe92521d6015
Jin Tian <today.xa@gmail.com>
2016-03-25 00:06:57 +0800

    已经实现在线人数，订单数，总金额 动态图形展示功能

--- server/rest.py
+++ server/rest.py
@@ -18,6 +18,8 @@ FLAG_ERROR_TYPE_INVALID = '{"flag":"error","error_no":"10002","msg":"Type invali
 
 urls = (
     '/onlineCount', 'onlineCount',
+    '/orderCount', 'orderCount',
+    '/orderSum', 'orderSum',
     "/(.*)", "blankPage"
 )
 
@@ -33,23 +35,54 @@ class onlineCount:
     def GET(self):
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Access-Control-Allow-Credentials', 'true')
-        return getCount()
+        return getOnlineCount()
 
-def getCount():
-    # 打开数据库连接
-    db = MySQLdb.connect("localhost","root","","eshop" )
+class orderCount:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Access-Control-Allow-Credentials', 'true')
+        return getOrderCount()
+
+class orderSum:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Access-Control-Allow-Credentials', 'true')
+        return getOrderSum()
+
+def getOnlineCount():
+    db = getConnect()
     # 使用cursor()方法获取操作游标 
     cursor = db.cursor()
     # 使用execute方法执行SQL语句
     cursor.execute("SELECT count(*) as onlineCount from nbt_customer_activity")
     # 使用 fetchone() 方法获取一条数据库。
     data = cursor.fetchone()
-    print "Database version : %s " % data
+    print "Database : %s " % data
     # 关闭数据库连接
     db.close()
     return "%s" % data
 
+def getOrderCount():
+    db = getConnect()
+    cursor = db.cursor()
+    cursor.execute("SELECT count(*) as orderCount from nbt_order")
+    data = cursor.fetchone()
+    print "Database : %s " % data
+    db.close()
+    return "%s" % data
 
+def getOrderSum():
+    db = getConnect()
+    cursor = db.cursor()
+    cursor.execute("SELECT sum(total) as orderCount from nbt_order")
+    data = cursor.fetchone()
+    print "Database : %s " % data
+    db.close()
+    return "%s" % data
+
+def getConnect():
+    # 打开数据库连接 
+    return MySQLdb.connect("localhost","root","","eshop" )
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
